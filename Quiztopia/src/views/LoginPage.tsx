240b689b67b88af27869406383052c8ef2aa745e
hellnomary <maria.porcires@outlook.com>
2023-08-29 13:22:02 +0200

    token

--- Quiztopia/src/views/LoginPage.tsx
+++ Quiztopia/src/views/LoginPage.tsx
@@ -12,7 +12,7 @@ function LoginPage() {
         message?: string;
        }
 
-    async function createUser() {
+    async function handleCreateUser() {
         const url = 'https://fk7zu3f4gj.execute-api.eu-north-1.amazonaws.com/auth/signup'
 
         const settings = {
@@ -26,18 +26,41 @@ function LoginPage() {
         const response = await fetch(url, settings)
         const data: ApiResponse = await response.json()
         console.log('data from user',data);
+    }
+
+    interface ApiSignUp {
+        success: boolean;
+        message?: string;
+        token?: string;
+      }
+    
+    async function handleLogIn() {
+        const url = 'https://fk7zu3f4gj.execute-api.eu-north-1.amazonaws.com/auth/login'
+
+        const settings = {
+            method: 'POST',
+            headers: { 'Content-Type' : 'application/json' },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            })
+        }
+        const response = await fetch(url, settings)
+        const data: ApiSignUp = await response.json()
+        console.log('nästa data: ', data);
         
     }
-    
 
     return(
         <section>
-            <input type="text" placeholder='Sign up' value={username}
+            <input type="text" placeholder='Användarnamn' value={username}
             onChange={event =>  setUsername(event.target.value)} />
 
-            <button onClick={createUser}>Sign up</button>
-            <input></input>
-            <button>Login</button>
+            <input type="text" placeholder='Lösenord' value={password}
+            onChange={event => setPassword(event.target.value)} />
+
+            <button onClick={ handleCreateUser }>Sign up</button>
+            <button onClick={ handleLogIn }>Login</button>
 
 
         </section>
