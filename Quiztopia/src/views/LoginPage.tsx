2018b2a429564e6e80137a48310555599f475926
hellnomary <maria.porcires@outlook.com>
2023-08-29 13:00:26 +0200

    signup

--- Quiztopia/src/views/LoginPage.tsx
+++ Quiztopia/src/views/LoginPage.tsx
@@ -7,22 +7,35 @@ function LoginPage() {
     const [username, setUsername] = useState<string>('')
     const [password, setPassword] = useState<string>('')
 
+    interface ApiResponse {
+        success: boolean;
+        message?: string;
+       }
+
     async function createUser() {
         const url = 'https://fk7zu3f4gj.execute-api.eu-north-1.amazonaws.com/auth/signup'
 
-        const setting = {
+        const settings = {
             method: 'POST',
+            headers: { 'Content-Type' : 'application/json' },
             body: JSON.stringify({
                 username: username,
                 password: password
             })
         }
+        const response = await fetch(url, settings)
+        const data: ApiResponse = await response.json()
+        console.log('data from user',data);
+        
     }
+    
 
     return(
         <section>
-            <input></input>
-            <button>Sign up</button>
+            <input type="text" placeholder='Sign up' value={username}
+            onChange={event =>  setUsername(event.target.value)} />
+
+            <button onClick={createUser}>Sign up</button>
             <input></input>
             <button>Login</button>
 
