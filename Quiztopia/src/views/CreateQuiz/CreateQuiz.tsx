24a8ec55b5d9ac00e631b5bc2f2255774a20f306
hellnomary <maria.porcires@outlook.com>
2023-08-31 14:48:54 +0200

    map

--- Quiztopia/src/views/CreateQuiz/CreateQuiz.tsx
+++ Quiztopia/src/views/CreateQuiz/CreateQuiz.tsx
@@ -4,6 +4,7 @@ import {handleCreatequiz} from './api'
 import { Position } from '../../interfaces';
 import { getPosition } from '../../geolocation';
 import mapboxgl, { Map as MapGl } from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibWFyaWFwb3JjaXJlcyIsImEiOiJjbGxwNjUwMnIwM2tqM3BwOG5idWxhd2lpIn0.lzqiA47Mt1DcZZw3cSfhRQ'
@@ -17,34 +18,34 @@ function CreateQuiz() {
     const [answer, setAnswer] = useState<string>('')
     const [position, setPosition] = useState<Position | null>(null)
     
-   const mapContainer = useRef(null)
-   const mapRef = useRef<MapGl | null>(null)
-   const [lat, setLat] = useState<number>(57.7)
-   const [lng, setLng] = useState<number>(11.89)
-   const [zoom, setZoom] = useState<number>(10)
+    const mapContainer = useRef(null)
+    const mapRef = useRef<MapGl | null>(null)
+    const [lat, setLat] = useState<number>(57.7)
+    const [lng, setLng] = useState<number>(11.89)
+    const [zoom, setZoom] = useState<number>(10)
 
-   useEffect(() => {
-    if( mapRef.current || !mapContainer.current ) return
+    useEffect(() => {
+        if( mapRef.current || !mapContainer.current ) return
 
-    mapRef.current = new MapGl({
-        container: mapContainer.current,
-        style: 'mapbox://styles/mapbox/streets-v12',
-        center: [lng, lat],
-        zoom: zoom
-    });
-    const map: MapGl = mapRef.current
+            mapRef.current = new MapGl({
+                container: mapContainer.current,
+                style: 'mapbox://styles/mapbox/streets-v12',
+                center: [lng, lat],
+                zoom: zoom
+            });
+            const map: MapGl = mapRef.current
 
-    map.on('move', () => {
-        interface Position {
-            lng: number;
-            lat: number;
-        }
-        const position: Position = map.getCenter()
-        setLat(Number(position.lat.toFixed(4)))
-        setLng(Number(position.lng.toFixed(4)))
-        setZoom(map.getZoom());
-    })
-}, [lat, lng, zoom])
+            map.on('move', () => {
+                interface Position {
+                    lng: number;
+                    lat: number;
+                }
+                const position: Position = map.getCenter()
+                setLat(Number(position.lat.toFixed(4)))
+                setLng(Number(position.lng.toFixed(4)))
+                setZoom(map.getZoom());
+            })
+    }, [lat, lng, zoom])
 
     return(
         <section>
@@ -66,6 +67,7 @@ function CreateQuiz() {
                 )} 
                   <button onClick={() => getPosition(setPosition)}> Var är jag? </button>
                   <p>Du är här! {position?.latitude} {position?.longitude}</p>
+                  <p> Center position: {lat} lat, {lng} lng </p>
                   <div ref={mapContainer} className="map-container" />
         </section>
     )
