21a2d372f4a1590ecff18c6738e67e746563995d
hellnomary <maria.porcires@outlook.com>
2023-09-07 11:55:14 +0200

    update

--- Quiztopia/src/views/CreateQuiz/CreateQuiz.tsx
+++ Quiztopia/src/views/CreateQuiz/CreateQuiz.tsx
@@ -12,6 +12,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibWFyaWFwb3JjaXJlcyIsImEiOiJjbGxwNjUwMnIwM2tqM
 console.log(mapboxgl.accessToken);
 
 
+
 function CreateQuiz() {
     const [showInput, setShowInput] = useState<boolean>(false)
     const [quizName, setQuizName] = useState<string>('')
@@ -23,55 +24,64 @@ function CreateQuiz() {
     const [lat, setLat] = useState<number>(57.7)
     const [lng, setLng] = useState<number>(11.89)
     const [zoom, setZoom] = useState<number>(10)
+    const [lngQuestion, setLngQuestion] = useState<number>()
+    const [latQuestion, setLatQuestion] = useState<number>()
+    const [addedQuestions, setAddedQuestions] = useState<ApiQuizResponseQuestions[]>([])
     
-
+    
+    
     useEffect(() => {
-
-    //getPosition()
-     
+        
+        
         if( mapRef.current || !mapContainer.current ) return
-
-            mapRef.current = new MapGl({
-                container: mapContainer.current,
-                style: 'mapbox://styles/mapbox/streets-v12',
-                center: [lng, lat],
-                zoom: zoom
-            });
-
-            const map: MapGl = mapRef.current
-           
-            map.on('move', () => {
-                interface Position {
-                    lng: number;
-                    lat: number;
-                }
-                const position: Position = map.getCenter()
-                setLat(Number(position.lat.toFixed(4)))
-                setLng(Number(position.lng.toFixed(4)))
-                setZoom(map.getZoom());
-
-                
-            })
-            map.on('click',(e)=>{
-                console.log('Click on map', e)
-                const marker = new mapboxgl.Marker()
-                    .setLngLat([e.lngLat.lng, e.lngLat.lat])
-                    .addTo(map);
-                // Du kanske behöver lägga till markers i en state-variabel senare (för att kunna ta bort markers)
-            })
+        
+        mapRef.current = new MapGl({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v12',
+            center: [lng, lat],
+            zoom: zoom
+        });
+        
+        const map: MapGl = mapRef.current
+        
+        map.on('move', () => {
+            interface Position {
+                lng: number;
+                lat: number;
+            }
+            const position: Position = map.getCenter()
+            setLat(Number(position.lat.toFixed(4)))
+            setLng(Number(position.lng.toFixed(4)))
+            setZoom(map.getZoom());
+            
+            
+        })
+        map.on('click',(e)=>{
+            console.log('Click on map', e)
+            const marker = new mapboxgl.Marker()
+            .setLngLat([e.lngLat.lng, e.lngLat.lat])
+            .addTo(map);
+            // Du kanske behöver lägga till markers i en state-variabel senare (för att kunna ta bort markers)
+            const lngLat = marker.getLngLat()
+            setLngQuestion(e.lngLat.lng)
+            setLatQuestion( e.lngLat.lat)
+            
+            console.log('lnglat klickat',lngLat);
+            
+        })
         
     }, [lat, lng, zoom])
-
-
+    
+    
     async function handleAddQuestion() {
-
+        
         const quizId = localStorage.getItem('quizId')
         console.log(quizId);
         
         const url ='https://fk7zu3f4gj.execute-api.eu-north-1.amazonaws.com/quiz/question'
         const token: string = localStorage.getItem('token') || ""
         console.log('JWTtoken: ', token)
-
+        
         const settings = {
             method: 'POST',
             body: JSON.stringify({
@@ -83,47 +93,65 @@ function CreateQuiz() {
                     latitude: lat,
                 }
             }),
-
+            
             headers: { 'Content-Type': 'application/json',
-             Authorization: `Bearer ${token}`},
+            Authorization: `Bearer ${token}`},
         }
-
+        
         const response = await fetch(url, settings)
         const data: QuestionsResponse = await response.json()
         console.log(data);
-        }
-
-
+        setAddedQuestions([ 
+            ...addedQuestions,
+            {
+                answer: answer,
+                location: {
+                    latitude: lat,
+                    longitude: lng,
+                },
+                question: question,
+            }
+        ])
+    }
+    
+    
     return(
         <section className='createPage'>
-            <section>
-               
-            <input className='create_input' type='text' placeholder='Namn på quiz' value={quizName} onChange={event => setQuizName(event.target.value)} />   
-                <button onClick={() => handleCreatequiz(setShowInput, quizName )}>Skapa quiz</button>
-                { showInput && (
-                    <div>
-                        <input className='create_input' placeholder='Fråga'
-                        value={question}
-                        onChange={(e) => setQuestion(e.target.value)}
-                        />
-                        <input className='create_input' placeholder='Svar'
-                        value={answer}
-                        onChange={(e) => setAnswer(e.target.value)}
-                        />
-
-                        <button onClick={handleAddQuestion}>Lägg till fråga</button>
-                    </div>
-                    
-                )} <button onClick={() => getPosition(mapRef.current, setPosition)}> Var är jag? </button>
-                <p>Du är här! {position?.latitude} {position?.longitude}</p>
-                 
-                 <p> Center position: {lat} lat, {lng} lng </p>
-                </section>
-                 
-                  <div ref={mapContainer} className="map-container" />
-        </section>
-    )
-
-}
-
-export default CreateQuiz
+        <section>
+        
+        <input className='create_input' type='text' placeholder='Namn på quiz' value={quizName} onChange={event => setQuizName(event.target.value)} />   
+        <button onClick={() => handleCreatequiz(setShowInput, quizName )}>Skapa quiz</button>
+        { showInput && (
+            <div>
+            <input className='create_input' placeholder='Fråga'
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
+            />
+            <input className='create_input' placeholder='Svar'
+            value={answer}
+            onChange={(e) => setAnswer(e.target.value)}
+            />
+            
+            <button onClick={handleAddQuestion}>Lägg till fråga</button>
+            <div>
+                {
+                    addedQuestions.map(q => (
+                        <div key={q.question}>{q.question}</div>
+                    ))
+                }
+            </div>
+            </div>
+            
+            )} <button onClick={() => getPosition(mapRef.current, setPosition)}> Var är jag? </button>
+            
+            
+            
+            </section>
+            
+            <div ref={mapContainer} className="map-container" />
+            </section>
+            )
+            
+        }
+        
+        export default CreateQuiz
